f08cb1959cb973c53633bddd100ce922a155336e
이건호 <hoyahoya0517@naver.com>
2024-01-11 19:11:09 +0900

    ver1.2

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -12,7 +12,7 @@ export default function NotFound() {
     <div className={styles.notFound}>
       <div className={styles.notFoundImg}>
         <img src="https://res.cloudinary.com/hoyahoya/image/upload/v1703133861/letter/basic-html-404-error_b0ch5d.webp" />
-        not
+        not이거 지우셈
       </div>
     </div>
   );
