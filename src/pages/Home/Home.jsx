54edaf33dc6265f1e6928a8c75d950870a8ff63c
이건호 <hoyahoya0517@naver.com>
2024-02-16 14:14:26 +0900

    verTestAws2.4

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import styles from "./Home.module.css";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { navOff } from "../../redux/redux";
+import Footer from "../../components/Footer/Footer";
 
 export default function Home() {
   const dispatch = useDispatch();
@@ -12,10 +13,34 @@ export default function Home() {
   }, []);
   return (
     <div className={styles.home}>
-      <div className={styles.mainVideo}>
-        <video autoPlay muted loop playsInline className={styles.video}>
-          <source src="https://res.cloudinary.com/hoyahoya/video/upload/v1707918941/letter/star4_alwwl0.mp4"></source>
-        </video>
+      <div className={styles.mainWrap}>
+        <div className={styles.main}>
+          <div className={styles.videoWrap}>
+            <div className={styles.starSpray}>
+              <span>STARSPRAY</span>
+            </div>
+            <video autoPlay muted loop playsInline className={styles.video}>
+              <source src="https://res.cloudinary.com/hoyahoya/video/upload/v1707918941/letter/star4_alwwl0.mp4"></source>
+            </video>
+          </div>
+          <div className={styles.videoWrap}>
+            <div className={styles.info}>
+              <span>PRODUCT</span>
+            </div>
+            <video autoPlay muted loop playsInline className={styles.video}>
+              <source src="https://res.cloudinary.com/hoyahoya/video/upload/v1708057452/video2_zrfgzb.mp4"></source>
+            </video>
+          </div>
+          <div className={styles.videoWrap}>
+            <div className={styles.info}>
+              <span>VINTAGE</span>
+            </div>
+            <video autoPlay muted loop playsInline className={styles.video}>
+              <source src="https://res.cloudinary.com/hoyahoya/video/upload/v1708060375/video33_dosgbd.mp4"></source>
+            </video>
+          </div>
+        </div>
+        <Footer />
       </div>
     </div>
   );
