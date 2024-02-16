139fc3613884d9b91a461ad84db34fcc6fb4b648
이건호 <hoyahoya0517@naver.com>
2024-02-16 15:04:31 +0900

    verTestAws2.7

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,44 +3,54 @@ import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { navOff } from "../../redux/redux";
 import Footer from "../../components/Footer/Footer";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
   const dispatch = useDispatch();
-
+  const navigate = useNavigate();
   useEffect(() => {
     window.scrollTo(0, 0);
     dispatch(navOff());
   }, []);
   return (
     <div className={styles.home}>
-      <div className={styles.mainWrap}>
-        <div className={styles.main}>
-          <div className={styles.videoWrap}>
-            <div className={styles.starSpray}>
-              <span>STARSPRAY</span>
-            </div>
-            <video autoPlay muted loop playsInline className={styles.video}>
-              <source src="https://res.cloudinary.com/hoyahoya/video/upload/v1708061272/letter/star4_zkdhdg.mp4"></source>
-            </video>
+      <div className={styles.main}>
+        <div className={styles.videoWrap}>
+          <div className={styles.starSpray}>
+            <span>STARSPRAY</span>
           </div>
-          <div className={styles.videoWrap}>
-            <div className={styles.info}>
-              <span>PRODUCT</span>
-            </div>
-            <video autoPlay muted loop playsInline className={styles.video}>
-              <source src="https://res.cloudinary.com/hoyahoya/video/upload/v1708061295/letter/video22_vhm7v7.mp4"></source>
-            </video>
-          </div>
-          <div className={styles.videoWrap}>
-            <div className={styles.info}>
-              <span>VINTAGE</span>
-            </div>
-            <video autoPlay muted loop playsInline className={styles.video}>
-              <source src="https://res.cloudinary.com/hoyahoya/video/upload/v1708061293/letter/video33_rsqybd.mp4"></source>
-            </video>
-          </div>
+          <video autoPlay muted loop playsInline className={styles.video}>
+            <source src="https://res.cloudinary.com/hoyahoya/video/upload/v1708061272/letter/star4_zkdhdg.mp4"></source>
+          </video>
         </div>
-        <Footer />
+        <div className={styles.videoWrap}>
+          <div className={styles.info}>
+            <span
+              onClick={() => {
+                navigate("/products/new");
+              }}
+            >
+              PRODUCT
+            </span>
+          </div>
+          <video autoPlay muted loop playsInline className={styles.video}>
+            <source src="https://res.cloudinary.com/hoyahoya/video/upload/v1708061295/letter/video22_vhm7v7.mp4"></source>
+          </video>
+        </div>
+        <div className={styles.videoWrap}>
+          <div className={styles.info}>
+            <span
+              onClick={() => {
+                navigate("/products/vintage");
+              }}
+            >
+              VINTAGE
+            </span>
+          </div>
+          <video autoPlay muted loop playsInline className={styles.video}>
+            <source src="https://res.cloudinary.com/hoyahoya/video/upload/v1708061293/letter/video33_rsqybd.mp4"></source>
+          </video>
+        </div>
       </div>
     </div>
   );
